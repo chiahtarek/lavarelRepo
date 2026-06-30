9ca231e1e52524047ee4c16eea8150500b9ab66b
chiahtarek <chiahtarek@gmail.com>
2026-06-29 21:03:44 -0300

    Auditing

--- laravel-crud-aula-02-master/app/Models/Cliente.php
+++ laravel-crud-aula-02-master/app/Models/Cliente.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use \Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Auditable as AuditableTrait;
 
-class Cliente extends Model
+class Cliente extends Model implements Auditable
 {
-     use SoftDeletes;
+    use AuditableTrait; 
+    use SoftDeletes;
 
     protected $fillable = [
         'nome',
